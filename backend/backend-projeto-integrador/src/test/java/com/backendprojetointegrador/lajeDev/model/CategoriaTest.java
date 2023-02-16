abf399bb93e269bdba71ca0acd86f9a03317c673
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2023-02-16 17:10:37 -0300

    refatoração do teste unitário de 'categoria' model

--- backend/backend-projeto-integrador/src/test/java/com/backendprojetointegrador/lajeDev/model/CategoriaTest.java
+++ backend/backend-projeto-integrador/src/test/java/com/backendprojetointegrador/lajeDev/model/CategoriaTest.java
@@ -1,6 +1,7 @@
 package com.backendprojetointegrador.lajeDev.model;
 
 
+import com.backendprojetointegrador.lajeDev.domain.model.Categorias;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,16 +9,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class CategoriaTest {
-    Categoria categoria;
-
+    Categorias categoria;
 
     @BeforeEach
     public void iniciaCategoria(){
-        categoria = new Categoria("Sedan");
+        categoria = new Categorias();
+        categoria.setQualificacao("Sedan");
+        categoria.setDescricao("veículo utilitário com quatro portas, maior espaço interno que um carro hatch, " +
+                "e bagageiro espaçoso");
+        categoria.setUrlImagem("https://uploads-ssl.webflow.com/5de0424183c9d7b00dd43bca/5f9071bf9bbc9f70d25745af_carros-sedan-mais-vendidos.jpg");
     }
 
     @Test
     public void verificaCategoria(){
-        assertEquals("Sedan", categoria.getNome());
+        assertEquals("Sedan", categoria.getQualificacao());
+        assertEquals("veículo utilitário com quatro portas, maior espaço interno que um carro hatch, " +
+                "e bagageiro espaçoso", categoria.getDescricao());
+        assertEquals("https://uploads-ssl.webflow.com/5de0424183c9d7b00dd43bca/5f9071bf9bbc9f70d25745af_carros-sedan-mais-vendidos.jpg",
+                categoria.getUrlImagem());
     }
 }
