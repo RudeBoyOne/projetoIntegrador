e11471adaa802929ed3f0c8250cf000410d643ab
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2023-02-16 17:09:38 -0300

    implementação do model de 'categorias'

---
+++ backend/backend-projeto-integrador/src/main/java/com/backendprojetointegrador/lajeDev/domain/model/Categorias.java
@@ -0,0 +1,22 @@
+package com.backendprojetointegrador.lajeDev.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Categorias {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String qualificacao;
+
+    private String descricao;
+
+    private String urlImagem;
+}
