1246ed59ad2d3973aad3b8667ad8720cab3ef4fc
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2023-02-16 22:13:16 -0300

    adição do 'EqualsAndHashCode'na classe Categoria

--- backend/backend-projeto-integrador/src/main/java/com/backendprojetointegrador/lajeDev/domain/model/Categoria.java
+++ backend/backend-projeto-integrador/src/main/java/com/backendprojetointegrador/lajeDev/domain/model/Categoria.java
@@ -5,13 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Categoria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     private String qualificacao;
     private String descricao;
