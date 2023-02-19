606f0bff3d1b92d3d194d98cda6c6268707787a1
ArivaldoCamargo <arivendaonline@gmail.com>
2023-02-19 17:33:38 -0300

    Criado header com logo, botão login e cadastro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-
+import Header from './components/header/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReactDom from 'react-dom';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Rent a Car</h1>
+    <Header/>
     </div>
   );
 }
