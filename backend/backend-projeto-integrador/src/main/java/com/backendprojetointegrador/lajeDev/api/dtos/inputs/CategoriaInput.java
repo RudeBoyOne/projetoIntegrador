a47237f2094beb8afe78be28f02a3664e28ac491
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2023-02-20 16:14:56 -0300

    implementação do Controller, Dtos e API de Categorias

---
+++ backend/backend-projeto-integrador/src/main/java/com/backendprojetointegrador/lajeDev/api/dtos/inputs/CategoriaInput.java
@@ -0,0 +1,13 @@
+package com.backendprojetointegrador.lajeDev.api.dtos.inputs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoriaInput {
+
+    private String qualificacao;
+    private String descricao;
+    private String urlImagem;
+}
