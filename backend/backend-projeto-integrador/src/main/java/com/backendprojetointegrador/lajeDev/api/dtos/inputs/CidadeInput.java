263d12c22a1bcca1d7e027b10ec3d49fff10a205
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2023-03-06 16:51:14 -0300

    Implementação das ações de CRUD para Entity 'Cidade'

---
+++ backend/backend-projeto-integrador/src/main/java/com/backendprojetointegrador/lajeDev/api/dtos/inputs/CidadeInput.java
@@ -0,0 +1,12 @@
+package com.backendprojetointegrador.lajeDev.api.dtos.inputs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CidadeInput {
+
+    private String nome;
+    private String pais;
+}
