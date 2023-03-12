0a971dfbc8d67da36c5a1b6b438827df801f991e
Jadson  Santos <jsantospereira007@gmail.com>
2023-03-12 18:00:01 -0300

    Criado o calendário e o botão reserva

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,12 @@ import Header from './components/header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReactDom from 'react-dom';
 import './index.css';
-import Booking from './components/booking/Booking';
 
 function App() {
   return (
     <div className="App">
-    {/* <Header/> */}
-    <Booking />
+    <Header/>
+    
     </div>
   );
 }
