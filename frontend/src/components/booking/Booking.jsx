03952b9a3c06884e1dbbbbe3df841a0dca821924
Jadson  Santos <jsantospereira007@gmail.com>
2023-03-12 17:46:00 -0300

    Criado o calendário e o botão reserva

--- frontend/src/components/booking/Booking.jsx
+++ frontend/src/components/booking/Booking.jsx
@@ -5,20 +5,35 @@ import 'react-calendar/dist/Calendar.css';
 
 const Booking = () => {
   const [value, onChange] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
   return (
     <div className={styles.booking}>
       <h1>Datas para reserva</h1>
       <div className={styles.dataRent}>
-        <Calendar onChange={onChange} value={value} />
-        <Calendar onChange={onChange} value={value} />
+        <Calendar className={styles.calendarRent} onChange={setDate} value={date} selectRange={true}/>
+        <Calendar className={styles.calendarBack} onChange={setDate} value={date} />
       </div>
       <div className={styles.boxBooking}>
         
-        <p>Adicione as data para reservar o seu carro e obter os preços exatos</p>
-        <button className={styles.button}>Iniciar reserva</button>
+        <p>Adicione a data para reservar o seu carro e obter os preços exatos.</p>
 
+        <button className={styles.button}>Iniciar reserva</button>
+        
+        {date.length > 0 ? (
+        <p className={styles.textCenter}>
+          <span className='bold'>Retirada:</span>{' '}
+          {date[0].toDateString()}
+          <span className='bold'>Entrega:</span> {date[1].toDateString()}
+        </p>
+      ) : (
+        <p className={styles.textCenter}>
+          <span className='bold'>Selecione a data de retirada e entrega:</span>{' '}
+          {date.toDateString()}
+        </p>
+      )}
       </div>
+      
     </div>
   );
 
